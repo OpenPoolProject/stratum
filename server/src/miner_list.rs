a013d8e9eb010ea49ef2f5454604d470610fead8
Sean Kilgarriff <sean@urkel.com>
2023-02-13 14:37:56 -0600

    fix: misc clippy updates

--- server/src/miner_list.rs
+++ server/src/miner_list.rs
@@ -15,21 +15,23 @@ impl MinerList {
         }
     }
 
-    pub async fn add_miner(&self, session_id: u32, miner: Miner) {
+    pub fn add_miner(&self, session_id: u32, miner: Miner) {
         self.miners.insert(session_id, miner);
     }
 
     //Returns the just removed miner (Mainly for Event reporting purposes)
     //If the miner exists otherwise returns None.
-    pub async fn remove_miner(&self, session_id: u32) -> Option<(u32, Miner)> {
+    #[must_use]
+    pub fn remove_miner(&self, session_id: u32) -> Option<(u32, Miner)> {
         self.miners.remove(&(session_id))
     }
 
-    pub async fn get_miner_by_id(&self, session_id: u32) -> Option<Miner> {
+    #[must_use]
+    pub fn get_miner_by_id(&self, session_id: u32) -> Option<Miner> {
         self.miners.view(&session_id, |_k, v| v.clone())
     }
 
-    pub async fn update_miner_by_session_id(&self, session_id: u32, miner: Miner) {
+    pub fn update_miner_by_session_id(&self, session_id: u32, miner: Miner) {
         self.miners.insert(session_id, miner);
     }
 }
