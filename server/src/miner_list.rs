614f81f49c09662a2cdc7cdc1d47ae66ba1282ef
Sean Kilgarriff <sean@urkel.com>
2023-02-13 14:32:49 -0600

    feat: replace rwlock<hashmap> with dashmap in miner_list

--- server/src/miner_list.rs
+++ server/src/miner_list.rs
@@ -1,39 +1,36 @@
 use crate::Miner;
-use std::{collections::HashMap, sync::Arc};
-use tokio::sync::RwLock;
+use dashmap::DashMap;
+use std::sync::Arc;
 
-//Use dashmap
-//@todo investigate better types at this -> I belive there is an async replacement for
-//RwLockHashmap.
 #[derive(Debug, Clone)]
 pub struct MinerList {
-    pub miners: Arc<RwLock<HashMap<u32, Miner>>>,
+    pub miners: Arc<DashMap<u32, Miner>>,
 }
 
 impl MinerList {
     #[must_use]
     pub fn new() -> Self {
         MinerList {
-            miners: Arc::new(RwLock::new(HashMap::new())),
+            miners: Arc::new(DashMap::new()),
         }
     }
 
     pub async fn add_miner(&self, session_id: u32, miner: Miner) {
-        self.miners.write().await.insert(session_id, miner);
+        self.miners.insert(session_id, miner);
     }
 
     //Returns the just removed miner (Mainly for Event reporting purposes)
     //If the miner exists otherwise returns None.
-    pub async fn remove_miner(&self, session_id: u32) -> Option<Miner> {
-        self.miners.write().await.remove(&(session_id))
+    pub async fn remove_miner(&self, session_id: u32) -> Option<(u32, Miner)> {
+        self.miners.remove(&(session_id))
     }
 
     pub async fn get_miner_by_id(&self, session_id: u32) -> Option<Miner> {
-        self.miners.read().await.get(&(session_id)).cloned()
+        self.miners.view(&session_id, |_k, v| v.clone())
     }
 
     pub async fn update_miner_by_session_id(&self, session_id: u32, miner: Miner) {
-        self.miners.write().await.insert(session_id, miner);
+        self.miners.insert(session_id, miner);
     }
 }
 
