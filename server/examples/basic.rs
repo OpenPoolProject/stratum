957b079b8152b57b36990a82ecfa678709319908
Sean Kilgarriff <sean@urkel.com>
2023-03-06 12:25:05 -0600

    fix: run cargo fmt

--- server/examples/basic.rs
+++ server/examples/basic.rs
@@ -1,4 +1,4 @@
-use std::{time::Duration};
+use std::time::Duration;
 use stratum_server::{Session, SessionList, StratumRequest, StratumServer};
 use tracing::subscriber::set_global_default;
 use tracing_subscriber::{fmt, prelude::*, EnvFilter, Registry};
